ec277cbb6c7c6d231bf6b5d7ce05cbb12fd6de7d
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-08-19 23:46:12 +0900

    Add artifact registry

--- terrafrom/main.tf
+++ terrafrom/main.tf
@@ -11,8 +11,17 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.credentials_file)
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_artifact_registry_repository" "ireco" {
+  provider = google-beta
+
+  location      = "asia-northeast1"
+  repository_id = "ireco"
+  description   = "ireco backend"
+  format        = "DOCKER"
+  project       = var.project
 }
